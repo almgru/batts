15c16b1712de48f3164ec6370bc43dab0deb7e43
Daniel Alm Grundström <almgru+github@fastmail.com>
2022-08-12 09:35:46 +0200

    Fix battery name in battery log

--- src/daemon.lua
+++ src/daemon.lua
@@ -1,6 +1,7 @@
 local glob = require('posix.glob').glob
 local sleep = require('posix.unistd').sleep
 local signal = require('posix.signal')
+local basename = require('posix.libgen').basename
 
 local daemon = {}
 
@@ -50,7 +51,8 @@ function daemon.start(sleep_interval_sec, battery_log_path)
          end
 
          local timestamp = os.date('%Y-%m-%d %H:%M:%S', os.time())
-         battery_log_file:write(timestamp .. ': ' .. bat_dir .. ', ' .. status .. ', ' .. capacity .. '%' .. '\n')
+         local battery = basename(bat_dir)
+         battery_log_file:write(timestamp .. ': ' .. battery .. ', ' .. status .. ', ' .. capacity .. '%' .. '\n')
 
          battery_log_file:close()
       end
