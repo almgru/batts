9b115f1e1baea8fcf2c0cbf21bb9ac570c5b7f2e
Daniel Alm Grundström <almgru+github@fastmail.com>
2022-08-12 18:45:07 +0200

    Calc (actual + extrapolated)mean discharge time

--- src/batstat.lua
+++ src/batstat.lua
@@ -24,6 +24,16 @@ elseif args.stats then
       }
    end
 
+   local function subrange(t, first, last)
+      local sub = {}
+
+      for i = first, last do
+         sub[#sub + 1] = t[i]
+      end
+
+      return sub
+   end
+
    local log_file, log_file_err = io.open('/home/almgru/.local/share/batstat/BAT1.log', 'r')
 
    if not log_file then
@@ -71,9 +81,42 @@ elseif args.stats then
       prev_line = line
    end
 
-   for k, v in pairs(discharge_durations) do
-      print('discharge duration ' .. k .. ' = ' .. v)
+   table.sort(discharge_durations)
+   local lower_half = #discharge_durations % 2 == 1
+       and subrange(discharge_durations, 1, math.floor(#discharge_durations / 2))
+       or subrange(discharge_durations, 1, #discharge_durations / 2)
+   local upper_half = #discharge_durations % 2 == 1
+       and subrange(discharge_durations, math.ceil(#discharge_durations / 2) + 1, #discharge_durations)
+       or subrange(discharge_durations, #discharge_durations / 2, #discharge_durations)
+   local lqr = #lower_half % 2 == 1
+       and lower_half[math.ceil(#lower_half / 2)]
+       or (lower_half[#lower_half / 2] + lower_half[#lower_half / 2 + 1]) / 2
+   local uqr = #upper_half % 2 == 1
+       and upper_half[math.ceil(#upper_half / 2)]
+       or (upper_half[#upper_half] / 2 + upper_half[#upper_half / 2 + 1]) / 2
+
+   local discharge_durations_without_outliers = {}
+
+   for _, v in ipairs(discharge_durations) do
+      if v >= lqr and v <= uqr then
+         table.insert(discharge_durations_without_outliers, #discharge_durations_without_outliers + 1, v)
+      end
    end
+
+   local sum = 0
+   for _, v in ipairs(discharge_durations_without_outliers) do
+      sum = sum + v
+   end
+
+   local mean_discharge_time_in_min = sum / #discharge_durations_without_outliers
+   local mean_discharge_time_h = math.floor((sum / #discharge_durations_without_outliers) / 60)
+   local mean_discharge_time_min = math.floor(((sum / #discharge_durations_without_outliers) % 60) + 0.5)
+   local extrapolated_h = math.floor((mean_discharge_time_in_min / 0.2) / 60)
+   local extrapolated_min = math.floor(((mean_discharge_time_in_min / 0.2) % 60) + 0.5)
+
+   print('mean discharge time: ' .. mean_discharge_time_h .. ' hours, ' .. mean_discharge_time_min .. ' minutes')
+   print('extrapolated mean full charge discharge time ' ..
+      extrapolated_h .. ' hours, ' .. extrapolated_min .. ' minutes')
 
 elseif args.daemon then
    if args.log_directory == '$XDG_DATA_HOME/batstat' then
