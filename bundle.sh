7e9faad8a8460b4c4242f6361496b24994ddbe69
Daniel Alm Grundström <almgru+github@fastmail.com>
2022-08-13 04:18:30 +0200

    Updates bundle script

--- bundle.sh
+++ bundle.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+# TODO: Read from rockspec
 VERSION=dev-1
 
 rm -rf dist
@@ -8,6 +9,7 @@ mkdir -p dist/batstat-"$VERSION"
 ./luarocks build
 
 cp -r lua_modules dist/batstat-"$VERSION"
+cp -r service dist/batstat-"$VERSION"
 
 cat <<EOF > dist/batstat-"$VERSION"/batstat
 #!/bin/sh
@@ -17,7 +19,7 @@ get_script_path() {
 
 script_path="\$(get_script_path)"
 
-LUA_PATH="\$script_path/lua_modules/share/lua/5.1/?.lua;\$script_path/lua_modules/share/lua/5.1/?/init.lua" LUA_CPATH="\$script_path/lua_modules/lib/lua/5.1/?.so;\$script_path/lua_modules/lib/lua/5.1/posix/?.so;\$script_path/lua_modules/lib/lua/5.1/posix/sys/?.so" /usr/bin/luajit "\$script_path"/lua_modules/lib/luarocks/rocks-5.1/batstat/dev-1/bin/batstat "\$@"
+LUA_PATH="\$script_path/lua_modules/share/lua/5.1/?.lua;\$script_path/lua_modules/share/lua/5.1/?/init.lua" LUA_CPATH="\$script_path/lua_modules/lib/lua/5.1/?.so;\$script_path/lua_modules/lib/lua/5.1/posix/?.so;\$script_path/lua_modules/lib/lua/5.1/posix/sys/?.so" exec /usr/bin/luajit "\$script_path"/lua_modules/lib/luarocks/rocks-5.1/batstat/dev-1/bin/batstat "\$@"
 EOF
 
 chmod +x dist/batstat-"$VERSION"/batstat
